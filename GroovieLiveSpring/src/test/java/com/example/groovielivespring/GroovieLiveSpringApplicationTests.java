4d7f9da271461ab3af95f947bae0fe5d92342281
MathisNIV <mathis.terrariose@gmail.com>
2023-12-21 10:43:46 +0100

    initial commit

---
+++ GroovieLiveSpring/src/test/java/com/example/groovielivespring/GroovieLiveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groovielivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovieLiveSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
