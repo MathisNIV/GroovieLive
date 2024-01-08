ed13d910c8ca7271b54b46f73dccbd4c65da5629
timothee-l <tim.levilly@gmail.com>
2024-01-08 16:56:16 +0100

    added endpoints

--- IA/recommendationService/recommendationService/app.py
+++ IA/recommendationService/recommendationService/app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/compare_songs', methods=['POST'])
+def compare_songs():
+    try:
+        data = request.get_json()
 
+        song1 = data.get('song1')
+        song2 = data.get('song2')
+
+        print(f"Song 1: {song1}")
+        print(f"Song 2: {song2}")
+
+        return jsonify({"message": "Songs received successfully"})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+@app.route('/compare_song_and_playlist', methods=['POST'])
+def compare_song_and_list():
+    try:
+        data = request.get_json()
+
+        song = data.get('song')
+        string_list = data.get('string_list')
+
+        print(f"Song: {song}")
+        print(f"Playlist: {string_list}")
+
+        return jsonify({"message": "Data received successfully"})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
     app.run()
