9ed35843e67c9058c57b275da8d121dd8ac55a60
AmandinePicci <amandine.piccinali@gmail.com>
2024-01-08 10:42:08 +0100

    route react app

---
+++ ReactJS/my-app/src/components/Node/Room.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+export const Room = (props) => {
+
+    return (
+        <div className="container">
+            <h3>Hello World</h3>
+        </div>
+    );
+}
