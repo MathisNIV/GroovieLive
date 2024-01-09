7bc1173ba1c0a94e3414dbb923c9d4a73aca7c38
Cortoz <corto.deschamps@club-internet.fr>
2024-01-09 15:27:45 +0100

    Fixed DTO project to dockerise GroovieSpring and added GroovieSpring docker file

--- DTO/src/test/java/com/example/dto/DtoApplicationTests.java
+++ DTO/src/test/java/com/example/dto/DtoApplicationTests.java
@@ -1,12 +1,7 @@
 package com.example.dto;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class DtoApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
