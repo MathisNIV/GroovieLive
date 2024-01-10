940ec9289c4f63cd4004a6b9d43ac0d7364c690e
AmandinePicci <amandine.piccinali@gmail.com>
2024-01-10 11:52:48 +0100

    react room

--- ReactJS/my-app/src/components/Room/Room.jsx
+++ ReactJS/my-app/src/components/Room/Room.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './Room.css';
-import {useParams, useSearchParams} from 'react-router-dom';
+import {useSearchParams} from 'react-router-dom';
+import {SearchSong} from "./SearchSong.jsx";
+import {Header} from "../Frameworks/Header.jsx";
+import {Footer} from "../Frameworks/Footer.jsx";
+import {Suggestions} from "./Suggestions.jsx";
 
 export const Room = (props) => {
     const socket = props.socket;
-    const [inputValue, setInputValue] = useState('');
-    const [songs, setSongs] = useState([]);
     const [params, setParams] = useSearchParams();
-
     const [listRooms, setListRooms] = useState([]);
     const [roomExist, setRoomExist] = useState(false);
     const id = params.get("id");
@@ -17,7 +18,7 @@ export const Room = (props) => {
         socket.on('roomsList', (entryList) => {
             setListRooms(entryList);
         })
-    }, []);
+    }, [id]);
 
     useEffect(() => {
         if (listRooms.includes(id)) {
@@ -28,55 +29,15 @@ export const Room = (props) => {
         }
     }, [listRooms, id])
 
-    useEffect(() => {
-        socket.on('songs', (songs) => {
-            setSongs(songs.slice(0, 10));
-        });
-        if (inputValue.trim()) {
-            socket.emit('msg', {
-                text: inputValue,
-            });
-        }
-    }, [inputValue, socket]);
-
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value);
-    };
-
-    const handleSongClick = (song) => {
-        setInputValue('');
-        console.log(song);
-        // socket.emit('song', {
-        //     song,
-        // });
-
-    }
-
     return (
         <div className="container">
+            <Header title ="Welcome to the party !"/>
             {roomExist &&
-            <div className="card">
-                <h3>Hello You! Welcome to the party</h3>
-                <div className="ui action input">
-                    <input
-                        type="text"
-                        value={inputValue}
-                        onChange={handleInputChange}
-                        placeholder="Rechercher une musique..."
-                    />
+                <div>
+                    <SearchSong socket={socket}/>
+                    <Suggestions />
                 </div>
-                <div className="song-list">
-                    <ul className="song-ul">
-                        {inputValue.trim() && songs.map((song, index) => (
-                            <div key={index}>
-                                <img src={song.imageUrl} />
-                                <li onClick={() => handleSongClick(song)} className="song-li">{song.title}, {song.author} ({song.mixTitle} version)</li>
-                            </div>
-                        ))}
-                    </ul>
-                </div>
-            </div>}
-
+                }
             {!roomExist &&
                 <h3>Your QR code is not valid ! Try again</h3>
             }
