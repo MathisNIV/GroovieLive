7598ba9ece95e154afde06b4e8b94410f5a6dd45
AmandinePicci <amandine.piccinali@gmail.com>
2024-01-10 17:21:44 +0100

    css sa maman

--- ReactJS/my-app/src/store.js
+++ ReactJS/my-app/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import songReducer from './slices/SongSlice.js';
+import userReducer from './slices/UserSlice.js';
 
 export default configureStore({
     reducer: {
         songReducer: songReducer,
+        userReducer: userReducer,
     },
 })
