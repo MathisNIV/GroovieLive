9369aef0254a433a03788029ae2f44fa748d3591
SarahLeCorre <sarah.lecorre@gmail.com>
2024-01-15 13:53:15 +0100

    je sais plus mais fallait commit

--- GroovieLiveSpring/src/main/java/com/example/groovielivespring/auth/controller/AuthRepo.java
+++ GroovieLiveSpring/src/main/java/com/example/groovielivespring/auth/controller/AuthRepo.java
@@ -1,7 +1,9 @@
 package com.example.groovielivespring.auth.controller;
 
+import com.example.groovielivespring.auth.controller.AuthRepo;
 import com.example.groovielivespring.auth.model.UserDB;
 import org.springframework.data.repository.CrudRepository;
+
 
 public interface AuthRepo extends CrudRepository<UserDB, Integer> {
 	public UserDB findByUsername(String username);
