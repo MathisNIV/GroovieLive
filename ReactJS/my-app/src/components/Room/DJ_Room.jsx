0eab0ed72a7a93ce1a7c155474331849a276ca65
MathisNIV <mathis.terrariose@gmail.com>
2024-01-16 14:20:54 +0100

    ca va

--- ReactJS/my-app/src/components/Room/DJ_Room.jsx
+++ ReactJS/my-app/src/components/Room/DJ_Room.jsx
@@ -13,10 +13,9 @@ export const DJ_Room = (props) => {
     const socket = props.socket;
 
     const CreationRoom = (e) => {
-        const response = await fetch('http://host.docker.internal:8081/api/hostip');
-        const data = await response.json();
-        console.log(data.ip);
-        return data.ip;
+        const response = fetch('http://host.docker.internal:8081/api/hostip')
+            .then(response => response.json())
+            .then(data => console.log(data.ip));
         e.preventDefault();
         socket.emit('createRoom', current_user);
         socket.on('roomUrl', (room) => {
