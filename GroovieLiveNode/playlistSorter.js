cad53856d7726b212026d9df50d243927df569b6
AmandinePicci <amandine.piccinali@gmail.com>
2024-01-17 10:38:53 +0100

    tracklist dj room

--- GroovieLiveNode/playlistSorter.js
+++ GroovieLiveNode/playlistSorter.js
@@ -2,11 +2,11 @@ const axios = require('axios');
 
 
 async function sort(playlist) {
-    console.log('unsorted: ' + JSON.stringify(playlist))
+    // console.log('unsorted: ' + JSON.stringify(playlist))
     try {
         let resp = await axios.post('http://nginx:8081/GroovieLiveFlask-api/sort/playlist',
                                     {'playlist': playlist});
-        console.log('sorted: ' + JSON.stringify(resp.data.playlist));
+        // console.log('sorted: ' + JSON.stringify(resp.data.playlist));
     } catch(error) {
         console.log(error);
     }
