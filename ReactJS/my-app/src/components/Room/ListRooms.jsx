92d5a03dc98b3a32c03054fa755da351238c46ee
SarahLeCorre <sarah.lecorre@gmail.com>
2024-01-17 15:58:54 +0100

    front

--- ReactJS/my-app/src/components/Room/ListRooms.jsx
+++ ReactJS/my-app/src/components/Room/ListRooms.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from "react-router-dom";
+import {Header} from "../Frameworks/Header.jsx";
+import {Footer} from "../Frameworks/Footer.jsx";
 
 export const ListRooms = (props) => {
     const socket = props.socket;
@@ -14,16 +16,18 @@ export const ListRooms = (props) => {
 
     return (
         <div>
-            <h3>List of rooms available : </h3>
-            <nav>
-                <ul>
-                    {listRooms.map((id, index) => (
-                        <li key={index}>
-                            <Link to={`http://localhost:8081/PartyRoom/?id=${id}`}>{id}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
+            <Header title="GroovieLive"/>
+                <h3>List of rooms available : </h3>
+                <nav>
+                    <ul>
+                        {listRooms.map((id, index) => (
+                            <li key={index}>
+                                <Link to={`http://localhost:8081/PartyRoom/?id=${id}`}>{id}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            <Footer/>
         </div>
     );
 }
