cd17ea51cb72dac4134c91c57c07fb29bf126cdc
Cortoz <corto.deschamps@club-internet.fr>
2024-01-22 15:02:02 +0100

    added Login link in index body

--- ReactJS/my-app/src/components/Index.jsx
+++ ReactJS/my-app/src/components/Index.jsx
@@ -1,6 +1,7 @@
 import {ListRooms} from "./Room/ListRooms.jsx";
 import {Header} from "./Frameworks/Header.jsx";
 import {Footer} from "./Frameworks/Footer.jsx";
+import {Link} from "react-router-dom";
 import "./Index.css";
 
 
@@ -11,7 +12,7 @@ export const Index = (props) => {
             <Header title="Welcome to GroovieLive"/>
             <div className="index">
                 <div className="index-left">
-                    <p>You are a Dj, login using your Beatport account</p>
+                    <p>You are a Dj, <Link to={'/login'}>Login</Link> using your Beatport account</p>
                 </div>
                 <div className="index-right">
                     <p>You are a raver, join one of the following rooms:</p>
